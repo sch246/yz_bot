dc88f89d5251ad47c3c209100de51d85afbdc2bf
sch246 <980001119@qq.com>
2022-11-07 20:46:25 +0800

    更新str_tools

--- _code/s3/str_tool.py
+++ _code/s3/str_tool.py
@@ -1,22 +1,24 @@
+import re
+
 def addtab(s: str, tab='    '):
-    '''同样适用于\\r\\n'''
+    '''适用于\\n，\\r\\n以及\\r'''
     s = tab + s
-    return s.replace('\n', '\n'+tab)
+    return re.sub(r'\r?\n|\r',lambda m:tab+m.group(), s)
 
 
 def deltab(s: str, tab='    '):
-    '''同样适用于\\r\\n'''
+    '''适用于\\n，\\r\\n以及\\r'''
     s = s[len(tab):]
-    return s.replace('\n'+tab, '\n')
+    return re.sub(tab+r'\r?\n|\r',lambda m:m.group()[len(tab):], s)
 
 
-def replace_dic(s: str, d: dict):
+def replace_by_dic(s: str, d: dict):
     for k, v in d.items():
         s = s.replace(k, v)
     return s
 
 
-def replace_dic_reverse(s: str, d: dict):
+def replace_by_dic2(s: str, d: dict):
     for k, v in d.items():
         s = s.replace(v, k)
     return s
