fbcd66f3177bfe2d12be99b4b9e3a08fa714d586
sch246 <980001119@qq.com>
2022-11-08 21:49:27 +0800

    修复tab

--- _code/s3/str_tool.py
+++ _code/s3/str_tool.py
@@ -3,13 +3,13 @@ import re
 def addtab(s: str, tab='    '):
     '''适用于\\n，\\r\\n以及\\r'''
     s = tab + s
-    return re.sub(r'\r?\n|\r',lambda m:tab+m.group(), s)
+    return re.sub(r'\r?\n|\r',lambda m:m.group()+tab, s)
 
 
 def deltab(s: str, tab='    '):
     '''适用于\\n，\\r\\n以及\\r'''
     s = s[len(tab):]
-    return re.sub(tab+r'\r?\n|\r',lambda m:m.group()[len(tab):], s)
+    return re.sub(r'\r?\n|\r'+tab,lambda m:m.group().rstrip(' '), s)
 
 
 def replace_by_dic(s: str, d: dict):
