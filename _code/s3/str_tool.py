30d154f51122ede41e3506ccf6a6e0ddba469840
sch246 <980001119@qq.com>
2022-11-09 17:54:13 +0800

    Update str_tool.py

--- _code/s3/str_tool.py
+++ _code/s3/str_tool.py
@@ -25,7 +25,7 @@ def replace_by_dic2(s: str, d: dict):
 
 
 
-re_stc = re.compile(r'{([^:},]*):([^}]+)}|{([^:},]+)}')
+re_stc = re.compile(r'{(\D[^:},]*)?:([^}]+)}|{(\D[^:},]*)}')
 
 def _gen_f(loc):
     names = set()
