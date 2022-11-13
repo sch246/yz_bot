ece1e6b809f1f3591f622aac0cde95bf14a2132e
sch246 <980001119@qq.com>
2022-11-13 11:54:38 +0800

    添加了自动重启

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 '''启动bot'''
 
 import subprocess,sys
+
+auto_reboot = 'auto_reboot' in sys.argv[1:]
 while True:
     out = subprocess.run([sys.executable,'./_code/main.py',*sys.argv[1:]])
     print('已退出，返回码为',out.returncode)
-    if out.returncode==233:
+    if out.returncode in [233,-6]: # 当python多线程写入中强制关闭时，返回是-6
         print('重启中...')
+        continue
+    if auto_reboot and out!=0:
+        print('自动重启中...')
         continue
     break
