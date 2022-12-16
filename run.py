e5f703ade3d3297a5764b49c528a1e1db9e23aab
sch246 <980001119@qq.com>
2022-12-16 13:28:03 +0800

    修改了参数，添加了log_only模式

--- run.py
+++ run.py
@@ -4,14 +4,17 @@ import subprocess
 import sys
 import time
 
-if '-h' in sys.argv[1:]:
+if any(c in sys.argv[1:] for c in ['-h','--help','help']):
     print('''接受的参数:
-    -h          显示本提示
-    auto_reboot 当出现异常时自动重启
-    debug       debug模式(并没有什么区别)''')
+    -h              显示本提示
+    -a              当出现异常时自动重启
+    --auto_reboot   同上
+    -l              仅记录log
+    --log_only      同上
+    debug           debug模式(并没有什么区别)''')
     exit()
 
-AUTO_REBOOT = 'auto_reboot' in sys.argv[1:]
+AUTO_REBOOT = any(c in sys.argv[1:] for c in ['--auto_reboot', '-a'])
 while True:
     out = subprocess.run([sys.executable,'./_code/main.py',*sys.argv[1:]], check=False)
     print('已退出，返回码为',out.returncode)
