bf344580ffdd20e322b24693eac2c8b512aaf44c
sch246 <980001119@qq.com>
2024-10-13 17:38:12 +0800

    w
    - 重写了.cpp命令
    - 增加了repl模块
      - 以此重写了.js命令并增加了.hs命令
    - 修改了jrlp的行为，现在每天每个群每个人都可以有自己的老婆

--- _code/bot/cmds/jrlp.py
+++ _code/bot/cmds/jrlp.py
@@ -1,22 +1,25 @@
 '''今日老婆'''
 import time
-from main import getgroupstorage, memberlist, getname, headshot, getran, cache
+from main import getgroupstorage, getstorage, memberlist, getname, headshot, getran, cache, CQ_at
 
 
 def run(_:str):
-    '''从群友中随机抽一个作为大家今天的老婆，群之间是独立的
+    '''从群友中随机抽一个作为自己今天的老婆，每个群独立
 注：鸽子院禁用了该功能
 格式:
 .jrlp'''
     group_id = cache.thismsg().get('group_id')
+    user_id = cache.thismsg().get('user_id')
     if group_id is None:
         return '不支持私聊'
     if group_id == 916083933:
-        return
+        return '应雨弓弓的要求禁用了这个功能'
     date = time.strftime('%y-%m-%d')
     data = getgroupstorage()
+    data.setdefault(user_id, {})
+    data = data[user_id]
     if data.get('jrlp_date')!=date:
         data['jrlp_date'] = date
-        member = getran(memberlist())
+        member = getran([m for m in memberlist() if not m['user_id']==user_id])
         data['jrlp'] = member['user_id']
-    return f'今天的群友老婆是\n{headshot(data["jrlp"])}\n{getname(data["jrlp"])}\n恭喜这位群友！'
+    return f'[CQ:at,qq={user_id}]今天的老婆是\n{headshot(data["jrlp"])}\n{getname(data["jrlp"])}！'
