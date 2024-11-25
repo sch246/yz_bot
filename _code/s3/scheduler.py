7545acfc408b9263a966533ba0603799e53c3901
sch246 <980001119@qq.com>
2024-11-25 14:06:59 +0800

    Update scheduler.py

--- _code/s3/scheduler.py
+++ _code/s3/scheduler.py
@@ -3,3 +3,11 @@ from apscheduler.schedulers.background import BackgroundScheduler
 scheduler = BackgroundScheduler()
 scheduler.start()
 
+
+import atexit
+
+def cleanup():
+    print("Shutting down scheduler...")
+    scheduler.shutdown()
+
+atexit.register(cleanup)
